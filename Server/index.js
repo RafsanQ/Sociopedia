ecb7425cc15605ae4a329462dabd1ae1bd91e43f
Rafsan <rafsan.quayes@gmail.com>
2023-07-12 20:28:22 +0600

    Mongoose user model and auth post url created

--- Server/index.js
+++ Server/index.js
@@ -9,6 +9,7 @@ import morgan from 'morgan';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { error } from 'console';
+import { register } from './controllers/auth.js';
 
 
 /* CONFIGURATIONS */
@@ -36,6 +37,10 @@ const storage = multer.diskStorage({
     }
 });
 const upload = multer({storage});
+
+
+/* Routes with files */
+app.post("/auth/register", upload.single("picture"), register);
 
 /* Mongoose */
 const PORT = process.env.PORT || 6001;
