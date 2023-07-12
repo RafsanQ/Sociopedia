2d4cea7a27eedbc5d439d85822cac16da070173f
Rafsan <rafsan.quayes@gmail.com>
2023-07-12 21:53:33 +0600

    addRemoveFriend function done

--- Server/controllers/userControllers.js
+++ Server/controllers/userControllers.js
@@ -47,8 +47,16 @@ export const addRemoveFriend = async (req, res) => {
         await user.save();
         await friend.save();
         
-        
+        const friends = await Promise.all(
+            user.friends.map((id) => User.findById(id))
+        )
+        const formattedFriends = friends.map(
+            ({ _id, firstName, lastName, occupation, location, picturePath }) => {
+                return { _id, firstName, lastName, occupation, location, picturePath };
+            }
+        );
 
+        res.status(200).json(formattedFriends);
     }catch(error){
         res.status(404).json({message: error.message});
     }
