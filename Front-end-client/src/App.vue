962003b0fb869d96ddb179e894a85397fa3b15d4
Rafsan <rafsan.quayes@gmail.com>
2023-07-17 13:51:03 +0600

    Savepoint: no errors. Routes all work

--- Front-end-client/src/App.vue
+++ Front-end-client/src/App.vue
@@ -12,7 +12,7 @@ import Navbar from './views/Navbar.vue';
         <RouterLink :to="{ name: 'home' }">Home</RouterLink>
         <RouterLink :to="{ name: 'about' }">About</RouterLink>
         <RouterLink :to="{ name: 'login' }">Login</RouterLink>
-        <RouterLink :to="{ name: 'profileView', params: { userId: '1235' } }">Profile View</RouterLink>
+        <RouterLink :to="{ name: 'profileView', params: { userId: '12345' } }">Profile View</RouterLink>
       </nav>
     </div>
   </header>
--- Front-end-client/src/views/ProfileView.vue
+++ Front-end-client/src/views/ProfileView.vue
@@ -7,7 +7,7 @@ const userId = route.params.userId; // read parameter id (it is reactive)
 <template>
   <div>
     <h1>This is the profile page</h1>
-    <H2>The user is {{ userId }}</H2>
+    <H2>The user id is {{ userId }}</H2>
   </div>
 </template>
 
