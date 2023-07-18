aea178e91a4ee0b776b30c685cae3a4f58105802
Rafsan <rafsan.quayes@gmail.com>
2023-07-18 18:13:32 +0600

    Made the basic store using pinia

---
+++ Front-end-client/src/stores/index.js
@@ -0,0 +1,54 @@
+import { defineStore } from 'pinia'
+
+export const useCentralStore = defineStore('centralStore', {
+    state: () => ({
+        mode: 'light',
+        user: null,
+        token: null,
+        posts: []
+    }),
+
+    getters: {
+
+    },
+
+    actions: {
+        setMode: () => {
+            this.mode = this.mode === 'light' ? 'dark' : 'light';
+        },
+
+        setLogin: (action) => {
+            this.user = action.payload.user;
+            this.token = action.payload.token;
+        },
+
+        setLogout: () => {
+            this.user = null;
+            this.token = null;
+        },
+
+        setFriends: (action) => {
+            if(this.user){
+                this.user.friends = action.payload.friends;
+            }
+            else{
+                console.error("User Friends non-existent");
+            }
+        },
+
+        setPosts: (action) => {
+            this.posts = action.payload.posts;
+        },
+
+        setPost: (action) => {
+            const updatedPosts = this.posts.map((post) => {
+                if(post._id === action.payload.post_id) {
+                    return action.payload.post;
+                }
+                return post;
+            })
+
+            this.post = updatedPosts;
+        }
+    }
+})
