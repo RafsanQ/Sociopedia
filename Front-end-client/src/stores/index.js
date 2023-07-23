d11ef66c8a0b7a2f20d069e739e71e3bc50b925f
Rafsan <rafsan.quayes@gmail.com>
2023-07-24 01:17:02 +0600

    Fixed the actions in the central store

--- Front-end-client/src/stores/index.js
+++ Front-end-client/src/stores/index.js
@@ -27,48 +27,42 @@ export const useCentralStore = defineStore('centralStore', () => {
         }
     }
 
+    const setLogin = (action) => {
+        this.user.value = action.payload.user;
+        this.token.value = action.payload.token;
+    }
+
+    const setLogout = () => {
+        this.user.value = null;
+        this.token.value = null;
+    }
+
+    const setFriends = (action) => {
+        if(this.user.value){
+            this.user.value.friends = action.payload.friends;
+        }
+        else{
+            console.error("User Friends non-existent");
+        }
+    }
+    
+    const setPosts = (action) => {
+        this.posts = action.payload.posts;
+    }
+
+    const setPost = (action) => {
+        const updatedPosts = this.posts.map((post) => {
+            if(post._id === action.payload.post_id) {
+                return action.payload.post;
+            }
+            return post;
+        })
+
+        this.post = updatedPosts;
+    }
 
 
-    // actions: {
-    //     setMode() {
-    //         this.mode = this.mode === 'light' ? 'dark' : 'light';
-    //     },
-
-    //     setLogin: (action) => {
-    //         this.user = action.payload.user;
-    //         this.token = action.payload.token;
-    //     },
-
-    //     setLogout: () => {
-    //         this.user = null;
-    //         this.token = null;
-    //     },
-
-    //     setFriends: (action) => {
-    //         if(this.user){
-    //             this.user.friends = action.payload.friends;
-    //         }
-    //         else{
-    //             console.error("User Friends non-existent");
-    //         }
-    //     },
-
-    //     setPosts: (action) => {
-    //         this.posts = action.payload.posts;
-    //     },
-
-    //     setPost: (action) => {
-    //         const updatedPosts = this.posts.map((post) => {
-    //             if(post._id === action.payload.post_id) {
-    //                 return action.payload.post;
-    //             }
-    //             return post;
-    //         })
-
-    //         this.post = updatedPosts;
-    //     }
-    // },
-    return { mode, user, token, count, setMode}
+    return { mode, user, token, count, setMode, setLogin, setLogout, setFriends, setPosts, setPost };
 },{
     persist: sessionStorage,
   },)
